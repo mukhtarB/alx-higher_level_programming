5534a5cffa58db877e8ac6e675fd4825e2580018
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-30 20:17:59 -0700

    program: replicate bytecode functionality

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+
+def magic_calculation(a, b):
+    """
+    Implement calc based on bytecode
+    """
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
+    return None
