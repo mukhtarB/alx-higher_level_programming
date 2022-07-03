274a8a708bb64f462432360236b0d25c171847e6
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-07-03 14:01:03 -0700

    program: task 8: multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     """Get the length of string and first char
-    of a sentence
-    
+    of a sentence.
+
     Args: sentence - The sentence to mentioned
 
     Return: Tuple with len and first char
     """
 
     if not sentence:
-        return (len(sentencs), None)
+        return (len(sentence), None)
     return (len(sentence), sentence[0])
