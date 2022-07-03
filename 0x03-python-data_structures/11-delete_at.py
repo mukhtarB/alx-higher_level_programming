992cb87f7e15e1d9c643b5d0a74d43f0a59b95b6
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-07-03 14:43:47 -0700

    prorgam: task 11: del item at specific index of list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,9 +4,10 @@ def delete_at(my_list=[], idx=0):
 
     Args: a List and an index number
 
-    Return: None
+    Return: my_list
     """
     if idx < 0 or idx >= len(my_list):
         return my_list
 
     del my_list[idx]
+    return my_list
