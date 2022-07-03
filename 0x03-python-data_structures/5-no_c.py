479b39a3f759bf7d3be389d2b3c967eac571a380
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-07-02 23:21:44 -0700

    program: task 5: func that removes all c and C characters from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    """Remove all 'c' and 'C' char from string.
+
+    Args: my_string: str to remove from
+
+    Return: new string
+    """
     new_string = ''
     for character in my_string:
         if character not in 'cC':
