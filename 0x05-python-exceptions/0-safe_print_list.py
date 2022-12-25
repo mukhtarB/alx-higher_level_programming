c416583a4b0b8e022d4a4341f30eaf5a827d3bc7
mukhtarB <mukhtar.b017@gmail.com>
2022-12-24 17:57:33 -0800

    program: print x el of a list of randoms

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+
+    for item in range(0, x):
+
+        try:
+            print("{}".format(my_list[item]), end='')
+
+            counter += counter
+
+        except IndexError:
+            pass
+
+    print()
+    return counter
