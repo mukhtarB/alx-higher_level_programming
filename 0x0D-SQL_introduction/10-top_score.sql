97714915a285107bef359b43cb2755ac880832e0
mukhtarB <mukhtar.b017@gmail.com>
2022-12-25 10:29:42 -0800

    script: displays records in order

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 --list all records fof table from second table and hbtn_0c_0 database
-SELECT `score`, `name`
-  FROM `second_table`
- ORDER BY `score` DESC;
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
