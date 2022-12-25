2ba33b0439a3051beeb1f3e82bec3172da249242
mukhtarB <mukhtar.b017@gmail.com>
2022-12-25 09:01:01 -0800

    script: drops db iff existing

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- a script that deletes the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 doesn’t exist, your script should not fail
-DROP DATABASE IF EXISTS htbn_0c_0;
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail.
+DROP DATABASE IF EXISTS hbtn_0c_0;
