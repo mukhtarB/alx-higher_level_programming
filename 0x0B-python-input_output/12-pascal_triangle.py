65c920aec1294876d5dc09ec426fe98d4ac18e34
mukhtarB <mukhtar.b017@gmail.com>
2022-12-25 23:55:56 -0800

    program: pascal's triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -19,7 +19,7 @@ def pascal_triangle(n):
     for term in range(3, n + 1):
         temp = []
         last_term = triangle[-1]
-        for i in range(len(last_term)- 1):
+        for i in range(len(last_term) - 1):
             sum_ = last_term[i] + last_term[i + 1]
             temp.append(sum_)
         temp.insert(0, 1)
