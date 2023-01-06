f0317529e907a9238b48169713856f46a4b50744
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2023-01-05 21:08:45 -0800

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Script that fetches https://intranet.hbtn.io/status
+Same as Task 0 but using request package
 """
+
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
+
+if __name__ == "__main__":
+    response = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print(f"\t- type: {type(text)}")
-    print(f"\t- content: {text}")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
